093ddc18e66d8ddc1f32bb7b6b041dd2171b5328
LloydDagoc <lloyddagoc@gmail.com>
2026-05-20 17:27:55 +0800

    practicing ed6 modules

---
+++ ESm modules practice/app.js
@@ -0,0 +1,4 @@
+import {greeting, farewell} from "./message";
+
+console.log(greeting);
+console.log(farewell);
---
+++ ESm modules practice/message.js
@@ -0,0 +1,3 @@
+export const greeting = "Hello Students";
+export const farewell = "Goodbye Students";
+
